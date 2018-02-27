5d1dffa97221e0212e773ef79d86c5c080d4fb15
ryu19j <ryu.19j.5@gmail.com>
2018-02-28 01:42:43 +0900

    iteratorの動作確認用テストクラス作成

---
+++ src/test/java/com/github/ryu19j/gofstudy/iterator/IteratorPatternTest.java
@@ -0,0 +1,29 @@
+package com.github.ryu19j.gofstudy.iterator;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IteratorPatternTest {
+
+	@Test
+	public void CharacterListに入力した内容がnextで取得できること() {
+		CharacterList characterList = new CharacterList(3);
+		characterList.add(new Character("リュウ"));
+		characterList.add(new Character("ケン"));
+		characterList.add(new Character("サガット"));
+
+		Iterator<Character> iter = characterList.iterator();
+
+		Character character1 = iter.next();
+		assertThat(character1.getName(), is("リュウ"));
+
+		Character character2 = iter.next();
+		assertThat(character2.getName(), is("ケン"));
+
+		Character character3 = iter.next();
+		assertThat(character3.getName(), is("サガット"));
+	}
+
+}
